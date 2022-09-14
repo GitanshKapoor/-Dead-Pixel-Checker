15c398de168cf736710e2470dcc00ddda29026f6
Gitansh Kapoor <72307552+GitanshKapoor@users.noreply.github.com>
2022-09-14 23:38:22 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    document.querySelector(".menuBlock").classList.toggle('menuCollapsed');
-}
-
-function checkWindowWidth() {
-    if (window.innerWidth < 1024)
-        document.querySelector(".menuBlock").classList.add('menuCollapsed');
-}
